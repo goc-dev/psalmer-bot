0845a52319e087a7a657e64ceba2904eb95ce525
goc-dev <belchurch.org@gmail.com>
2025-04-05 18:00:08 +0000

    7: upd: "~Command" for not-command messages

--- engine/psalmer-bot.py
+++ engine/psalmer-bot.py
@@ -40,19 +40,14 @@ Next text, Next text
     message.reply(s_answer, parse_mode = "Markdown")
 
 
-@dp.message()
-async def echo_handler(message: Message) -> None:
+@router.message(~Command())
+async def handle_message_and_echo(message: Message) -> None:
     """
     Handler will forward receive a message back to the sender
 
     By default, message handler will handle all message types (like a text, photo, sticker etc.)
     """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+    await message.send_copy(chat_id=message.chat.id)
 
 
 dp.include_router(router)
