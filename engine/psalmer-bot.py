5b73d2dd67c8a9989dd08d9996a59c1b3e208b76
goc-dev <belchurch.org@gmail.com>
2025-04-12 06:09:56 +0000

    9: move the text into a file

--- engine/psalmer-bot.py
+++ engine/psalmer-bot.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 import sys
-from os import getenv
+from os import os, getenv
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, Router, html
 from aiogram.client.default import DefaultBotProperties
@@ -32,34 +32,24 @@ async def handle_command_start(message: Message) -> None:
 async def handle_command_psalm(message: Message) -> None:
     """This handler is for the command `/psalm #id to print text/chords of Psalm#id"""
 
-    s_answer = """
-*Есть победа*
-Тональность: `E`
-```Вступление
-E/H H7 E A E
-```
-```Куплет
-       A     G7     C#m
-Есть по|беда |для ме|ня,
-       E     F#7    Hsus H7
-Есть по|беда |для те|бя  |
-          E       Hm   A       D7
-Есть в Кро|ви Свят|ого |Агнца, |
-       E/H   H7     E7   A  E
-Есть по|беда |для ме|ня. |  |
-```
-```Припев
-   E   A/H  E         
-Мы |по-|--бе|дим, 
-   C#/D# G#7  C#m   E7
-мы |по---|--бе|дим, |
-     A     F#    E/H    C#7 
-Крови|ю Свя|того |Агнца |   
-F#m  E/H H7  E     A  E
-|мы  |по-|бе-|дим. |  |
-```
-"""
-    await message.answer(s_answer, parse_mode = "Markdown")
+    v_book_dir = '../hymnal/goc-2021/'
+    v_song_id  = 66
+    v_song_idx = str(v_song_id)
+    v_song_file = ''
+
+    # Iterate through files in the directory and find matching prefix
+    for filename in os.listdir(v_book_dir):
+        if filename.startswith(v_song_idx):
+            v_song_file = filename
+            break
+
+    if '' == v_song_file:
+        v_song_text_md = 'File not found'
+    else:
+        with open( v_song_file, 'r') as song_file:
+            v_song_text_md = song_file.read()
+
+    await message.answer(v_song_text_md, parse_mode = "Markdown")
 
 
 @router.message()
