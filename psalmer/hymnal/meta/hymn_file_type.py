65c8b4cf5afc7543f30253c3af69b671edecbbf6
goc-dev <belchurch.org@gmail.com>
2026-03-01 08:59:45 +0300

    44 - additional file types

--- psalmer/hymnal/meta/hymn_file_type.py
+++ psalmer/hymnal/meta/hymn_file_type.py
@@ -1,8 +1,11 @@
 from enum import Enum
 
 class HymnFileType(str, Enum):
-    MD_V2 = 'mdv2',
-    PDF   = 'pdf',
-    TXT   = 'txt',
-    MP3   = 'mp3',
+    """Enumeration of supported hymn file types."""
+    MD_V2 = 'mdv2'
+    PDF   = 'pdf'
+    TXT   = 'txt'
+    MP3   = 'mp3'
     OGG   = 'ogg'
+    JPEG  = 'jpeg'
+    PNG   = 'png'
